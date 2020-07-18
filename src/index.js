ef852d244ab104030e49a496ce8bceb84bbf8a57
Donald Knieriem <donald.knieriem@temple.edu>
2020-07-17 22:53:31 -0400

    1D

--- src/index.js
+++ src/index.js
@@ -1,71 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <input type="checkBox" className="square" onClick={() =>
-      	this.props.onClick()} checked={this.props.value} />
-    );
-  }
+import './assets/main.css';
+
+function Square(props) {
+  return (
+    <input type="checkBox" className="square w-5 h-5 rounded-none bg-white border-0" onClick={ props.onClick } checked={ props.value } disabled />
+  );
 }
 
 class Advancer extends React.Component {
 	render() {
 		return (
-			<button className="advancer" onClick={ () => this.props.onClick() } >
+			<button className="advancer py-2 px-4 m-2 bg-blue-300 rounded-md hover:bg-red-300 transition duration-500 focus:bg-red-500" onClick={ () => this.props.onClick() }>
 			Advance
 			</button>//
 	  );
 	}
 }
 
-class Board extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-      frame: 0,
-      squares: Array(props.width).fill(false).fill(true, props.width / 2, props.width / 2 + 1),
-		};
-	}
+class RuleInput extends React.Component {
+  render() {
+    return (
+      <label className="py-2 px-4 m-2">Rule <input type="text" className="ruleInput w-10" value={this.props.value} onChange={this.props.onChange} />
+      </label>
+    );
+  }
+}
 
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		squares[i] = ! squares[i];
-		this.setState({ squares: squares });
-	}
+class Board extends React.Component {
 
   renderSquare(i) {
     return (
     	<Square key={i.toString()}
-    		value={this.state.squares[i]} 
-    		onClick={() => this.handleClick(i)}
-    	/>
+    		value={this.props.squares[i]} 
+    	/>//
     );
-  }
-
-  advance(){
-    this.setState((state, props) => ({ 
-      frame: state.frame + 1,
-    }));
   }
 
   render() {
     
-    const list = this.state.squares.map((value, index) => {
+    const list = this.props.squares.map((value, index) => {
               return this.renderSquare(index);
               });
 
     return (
         <div className="board">
-          <div className="controls"> 
-            <div className="status">{this.state.frame}</div><Advancer onClick={() => this.advance()} />
+          <div className="boxList flex justify-center">
+            {list} 
           </div>
-           {list} 
-        </div>
+        </div>//
     )
   }
+}
+
+function applyRule(neighbors, rule)
+{
+
+  if( !Array.isArray(neighbors) || neighbors.length !== 3 )
+  {
+    return false;
+  }
+
+  // Rule 27 = 16 + 8 + 2 + 1 
+  // 0 0 0 1 1 0 1 1
+  const neighborNum = neighbors[0] * 4 + neighbors[1] * 2 + neighbors[2];
+  //0 + 0 + 0 = 0 
+  //2 ^ 0 = 1
+  
+  const match = Math.pow( 2, neighborNum ) & rule;
+  //console.log("ApplyRule: " , neighborNum, match);
+  return match;
 }
 
 class Game extends React.Component {
@@ -76,30 +81,82 @@ class Game extends React.Component {
       history: [{
         squares: Array(props.width).fill(false).fill(true, props.width / 2, props.width / 2 + 1),
       }],
+      frame: 0,
+      squares: Array(props.width).fill(false).fill(true, props.width / 2, props.width / 2 + 1),
+      rule: this.props.rule,
     };
+
+    //check rules
+    
+   //console.log("Rule #" + this.props.rule);
+
   }
+
+  advance(){
+
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const oldSquares = current.squares;
+    //console.log(oldSquares);
+
+    const newSquares = oldSquares.map( (x, index) => {
+      
+      if( index === 0 || index  === this.props.width - 1 ){
+        return false;
+      }
+
+      var neighbors = [ oldSquares[index - 1], oldSquares[index], oldSquares[index+1] ];
+      //console.log(neighbors);
+      var newVal = applyRule( neighbors, this.props.rule );
+      
+      //console.log(x, index, newVal);
+
+      return newVal;
+       
+    });
+      
+
+    this.setState((state, props) => ({ 
+      frame: state.frame + 1,
+      history: history.concat([{
+        squares: newSquares,
+      }]),
+    }));
+
+  }
+
+  newRule( event ){
+    this.setState({ rule: event.target.value });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
 
     return (
-      <div className="game">
+      <div className="game mt-10 container mx-auto">
+          <div className="controls flex flex-row bg-gray-200"> 
+            <RuleInput value={this.state.rule} onChange={ this.newRule.bind(this) } />
+            <div className="status py-2 px-4 m-2">Step: {this.state.frame}</div>
+            <Advancer onClick={() => this.advance()} />
+          </div>
         <div className="game-board">
-          <Board width={this.props.width} />
+          <Board squares={ current.squares } />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
-      </div>
+      </div>//
     );
   }
 }
 
 // ========================================
-
+// Rule 27 = 16 + 8 + 2 + 1 
+// 0 0 0 1 1 0 1 1
 ReactDOM.render(
-  <Game width={9} />,
+  <Game width={50} rule={27} />,
   document.getElementById('root')
 );
 
